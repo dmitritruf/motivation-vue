b14058fc9bd32c6890eaf18720a13703afad5f53
dmitritruf <gttim2@gmail.com>
2021-06-11 14:29:01 +0200

    #1 Set up initial Laravel project with Sanctum and Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,10 @@ import Vue from "vue";
 import App from "./App.vue";
 
 //Import store
-import store from "./store";
+import store from "./store/store";
 
 //Import router
-import router from "./router";
+import router from "./router/router";
 
 new Vue({
     el: "#app",
