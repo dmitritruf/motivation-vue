6c9a00f1bbbe7ff665ea64b5b4472624d890a87b
dmitritruf <gttim2@gmail.com>
2021-06-11 14:02:22 +0200

    Vue files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-require('./bootstrap');
+require("./bootstrap");
+import Vue from "vue";
+
+//Main pages
+import App from "./App.vue";
+
+//Import store
+import store from "./store";
+
+//Import router
+import router from "./router";
+
+new Vue({
+    el: "#app",
+    store,
+    router,
+    render: (h) => h(App),
+});
