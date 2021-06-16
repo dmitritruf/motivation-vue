b60185b44db3ca7ef453bfac0ad62dc6994fa06a
dmitritruf <gttim2@gmail.com>
2021-06-16 14:33:31 +0200

    Mutation added to set user and getter to get user

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -4,15 +4,26 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    getters: {},
+    state: {
+        user: null,
+    },
+    mutations: {
+        setUser(state, user) {
+            state.user = user;
+        },
+    },
+    getters: {
+        getUser: (state) => {
+            return state.user;
+        }
+    },
     actions: {
 
         login: ({ commit }, user) => {
             axios.post('/login', user).then(function (response) {
                 console.log('success');
                 console.log(response);
+                commit('setUser', response.data);
             }).catch(function (error) {
                 console.log(error);
             });
