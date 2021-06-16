7c19041f8b0e8dbf9c24dabd9802e6cef5a912a9
dmitritruf <gttim2@gmail.com>
2021-06-16 13:42:01 +0200

    Revert "Homepage layout"
    This reverts commit 1adc31fb6b10a988d28044382bd2063dd1168e74.

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,43 +1,6 @@
 <template>
     <div>
-        <div class="home-grid">
-            <div class="task-lists">
-                <div class="task-list">
-                    <h3>Task list 1</h3>
-                    <p>Dummy text</p>
-                    <p>Dummy text</p>
-                    <p>Dummy text</p>
-                </div>
-                <div class="task-list">
-                    <h3>Task list 2</h3>
-                    <p>Dummy text</p>
-                    <p>Dummy text</p>
-                    <p>Dummy text</p>
-                </div>
-                <div class="task-list">
-                    <h3>New Task list </h3>
-                </div>
-            </div>
-
-            <div class="character-tab">
-                <div class="character">
-                    <h3>Character</h3>
-                    <p>Dummy text</p>
-                    <p>Dummy text</p>
-                    <p>Dummy text</p>
-                </div>
-            </div>
-
-            <div class="friends-list">
-                <div class="friends">
-                    <h3>Friends</h3>
-                    <p>Dummy text</p>
-                    <p>Dummy text</p>
-                    <p>Dummy text</p>
-                </div>
-            </div>
-        </div>
-        
+        <h2>Home</h2>
     </div>
 </template>
 
@@ -50,50 +13,5 @@ export default {
 
 
 <style>
-    .home-grid {
-        display: grid;
-        column-gap: 25px;
-    }
-    .home-grid div div{
-        border: 2px solid teal;
-        border-radius: 20px;
-        margin: 10px;
-    }
-    .task-lists{
-        grid-row: 1/5;
-        grid-column: 1;
-        display: flex;
-        flex-wrap: wrap;
-    }
-    .task-lists div{
-        width: 45%;
-    }
-    .character-tab{
-        grid-row: 1;
-        grid-column: 2;
-    }
-    .friends-list{
-        grid-row:2;
-        grid-column: 2;
-    }
 
-    @media (max-width:730px){
-        .task-lists{
-            grid-row: 1;
-            grid-column: 1;
-            display: flex;
-            flex-wrap: wrap;
-        }
-        .character-tab{
-            grid-row: 2;
-            grid-column: 1;
-        }
-        .friends-list{
-            grid-row:3;
-            grid-column: 1;
-        }
-        .task-lists div{
-            width: 100%;
-        }
-    }
 </style>
