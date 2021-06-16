3bb1d0b44d3d989aa7116d6f57b4438ca9aa7b2a
dmitritruf <gttim2@gmail.com>
2021-06-16 11:30:47 +0200

    Routes set

--- resources/js/router/router.js
+++ resources/js/router/router.js
@@ -8,6 +8,34 @@ export default new VueRouter({
         {
             path: "/",
             component: require('../pages/Home.vue').default,
+        },
+        {
+            path: "/login",
+            component: require('../pages/Login.vue').default,
+        },
+        {
+            path: "/register",
+            component: require('../pages/Register.vue').default,
+        },
+        {
+            path: "/character",
+            component: require('../pages/Character.vue').default,
+        },
+        {
+            path: "/notifications",
+            component: require('../pages/Notifications.vue').default,
+        },
+        {
+            path: "/settings",
+            component: require('../pages/Settings.vue').default,
+        },
+        {
+            path: "/achievements",
+            component: require('../pages/Achievements.vue').default,
+        },
+        {
+            path: "/profile/:id",
+            component: require('../pages/Profile.vue').default,
         }
     ]
 });
