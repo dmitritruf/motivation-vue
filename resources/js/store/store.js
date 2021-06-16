d4e728614f96869fd7ddb2325c38f3af661ee79b
dmitritruf <gttim2@gmail.com>
2021-06-16 15:08:43 +0200

    Register page with store axios call

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,5 +1,7 @@
+import axios from "axios";
 import Vue from "vue";
 import Vuex from "vuex";
+import router from '../router/router.js';
 
 Vue.use(Vuex);
 
@@ -21,9 +23,19 @@ export default new Vuex.Store({
 
         login: ({ commit }, user) => {
             axios.post('/login', user).then(function (response) {
-                console.log('success');
                 console.log(response);
                 commit('setUser', response.data);
+                router.push('/');
+            }).catch(function (error) {
+                console.log(error);
+            });
+        },
+
+        register: (_, user) => {
+            console.log(user);
+            axios.post('/register', user).then(function (response) {
+                console.log(response);
+                router.push('/login');
             }).catch(function (error) {
                 console.log(error);
             });
