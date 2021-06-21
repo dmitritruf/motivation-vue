b43fba5189b2999e371894a7794acb43bc3c39a6
dmitritruf <gttim2@gmail.com>
2021-06-21 11:20:18 +0200

    Added interceptor with Information Block to show errors

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,17 +2,20 @@
     <div>
         <navigation></navigation>
         <div class="content">
+            <information-block></information-block>
             <router-view></router-view>
         </div>
         <footer-comp></footer-comp>
     </div>
 </template>
+
 <script>
 import Navigation from './components/Navigation.vue';
 import FooterComp from './components/FooterComp.vue';
+import InformationBlock from './components/InformationBlock.vue';
 
 export default {
-    components: { Navigation, FooterComp },
+    components: { Navigation, FooterComp, InformationBlock },
     
 };
 </script>
