90909a19246ae547d9539a7c5746b41bedcd115e
dmitritruf <gttim2@gmail.com>
2021-06-23 13:46:00 +0200

    Show Task within TaskList on Home page. Also show sub-tasks

--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -1,15 +1,20 @@
 <template>
     <div>
         <h3>{{taskList.name}}</h3>
-        <ul>
-            <li v-for="task in taskList.tasks" :key="task.id">{{task.name}}</li>
-        </ul>
+        <template v-for="task in taskList.tasks">
+            <task :task="task" :key="task.id" class="task border"></task>
+        </template>
+        <div>
+            <p>Add new task</p>
+        </div>
     </div>
 </template>
 
 
 <script>
+import Task from './Task.vue';
 export default {
+    components: {Task},
     props: {
         taskList: Object,
     },
@@ -18,5 +23,17 @@ export default {
 
 
 <style>
-
+.task, .sub-task{
+    margin:1px;
+}
+.sub-task{
+    margin-left: 5px !important;
+}
+.task-description{
+    font-size:12px;
+}
+.task-title{
+    font-size:14px;
+    font-weight:700;
+}
 </style>
