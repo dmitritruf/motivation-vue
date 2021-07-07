b553003d4e74b122eec520ad3bcff939db99f5cb
dmitritruf <gttim2@gmail.com>
2021-07-07 15:22:25 +0200

    Changes to how user data (friends/character) is sent to front-end

---
+++ app/Http/Resources/FriendResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FriendResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'friend' => $this->full_display_name,
+            'friends_since' => $this->pivot->created_at->toDateString(),
+        ];
+    }
+}
