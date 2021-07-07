ce291c48db74d6599a3209555cd72ea70727b55d
dmitritruf <gttim2@gmail.com>
2021-07-07 15:44:04 +0200

    Start of a friend list

---
+++ resources/js/components/FriendsSummary.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="friends">
+            <span class="frame-title">Friends</span>
+            <div class="side-border bottom-border">
+                <ul>
+                    <li v-for="(value, index) in user.friends" :key="index">{{value.friend}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import {mapGetters} from 'vuex';
+export default {
+    mounted(){
+    },
+    methods: {
+    },
+    computed: {
+        ...mapGetters({
+            user: 'getUser',
+        }),
+    },
+}
+</script>
+
+
+<style>
+
+</style>
