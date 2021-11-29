3a881b6fd4e9b4e72b614f01f739e158511d29f8
dmitritruf <gttim2@gmail.com>
2021-11-29 15:41:00 +0100

    Changed Settings to b-form

--- resources/js/pages/Settings.vue
+++ resources/js/pages/Settings.vue
@@ -1,80 +1,89 @@
 <template>
-    <div>
+    <div class="w-60 center">
         <div class="form-title">
             <h3>Settings</h3>
         </div>
-
-<!-- //TODO Remake into b-form -->
-        <form @submit.prevent="submitSettings" v-if="!loading">
+        <b-form @submit.prevent="submitSettings" v-if="!loading">
             <h4>Profile settings</h4>
-            <div class="form-group">
-                <label for="rewards">Rewards type</label>
-                <select
+            <b-form-group
+                label="Rewards type"
+                label-for="rewards">
+                <b-form-select
                     name="rewards"
                     id="rewards"
-                    v-model="settings.rewards">
-                    <option value="CHARACTER">Character</option>
-                </select>
-            </div>
-            <div class="form-group">
+                    v-model="settings.rewards"
+                    :options="rewardTypes">
+                </b-form-select>
+                <base-form-error name="rewards" /> 
+            </b-form-group>
+            <b-form-group>
                 <b-form-checkbox
                     name="show_character"
                     id="show_character"
                     v-model="settings.show_character"
-                    >
+                    switch>
                     Show character on profile
                 </b-form-checkbox>
+                <base-form-error name="show_character" /> 
                 <b-form-checkbox
                     name="show_achievements"
                     id="show_achievements"
                     v-model="settings.show_achievements"
-                    >
+                    switch>
                     Show achievements on profile
                 </b-form-checkbox>
+                <base-form-error name="show_achievements" /> 
                 <b-form-checkbox
                     name="show_friends"
                     id="show_friends"
                     v-model="settings.show_friends"
-                    >
+                    switch>
                     Show friends on profile
                 </b-form-checkbox>
-            </div>
+                <base-form-error name="show_friends" /> 
+            </b-form-group>
 
             <div class="form-group">
                 <b-button type="submit" block>Save settings</b-button>
             </div>
-        </form>
+        </b-form>
         <hr />
         <form @submit.prevent="submitPasswordSettings" v-if="!loading">
             <h4>Change password</h4>
-            <p class="silent">If successful, you will be logged out. Please log in with your new password.</p>
-            <div class="form-group">
-                <label for="old_password">Old password</label>
-                <input 
+            <p class="text-muted">If successful, you will be logged out. Please log in with your new password.</p>
+            <b-form-group
+                label="Old password"
+                label-for="old_password">
+                <b-form-input 
                     type="password" 
                     id="old_password" 
                     name="old_password" 
                     placeholder="Old password" 
                     v-model="passwordSettings.old_password" />
-            </div>
-            <div class="form-group">
-                <label for="password">New password</label>
-                <input 
+                <base-form-error name="old_password" /> 
+            </b-form-group>
+            <b-form-group
+                label="New password"
+                label-for="password">
+                <b-form-input 
                     type="password" 
                     id="password" 
                     name="password" 
                     placeholder="New password" 
                     v-model="passwordSettings.password" />
-            </div>
-            <div class="form-group">
-                <label for="password_confirmation">Repeat new password</label>
-                <input 
+                <base-form-error name="password" /> 
+            </b-form-group>
+            <b-form-group
+                label="Repeat new password"
+                label-for="password_confirmation">
+                <b-form-input 
                     type="password" 
                     id="password_confirmation" 
                     name="password_confirmation" 
                     placeholder="Repeat password" 
                     v-model="passwordSettings.password_confirmation" />
-            </div>
+                <base-form-error name="password_confirmation" /> 
+            </b-form-group>
             
             <div class="form-group">
                 <b-button type="submit" block>Update password</b-button>
@@ -85,16 +94,18 @@
 
         <form @submit.prevent="submitEmailSettings" v-if="!loading">
             <h4>Change e-mail</h4>
-            <div class="form-group">
-                <label for="email">Change e-mail</label>
+            <b-form-group
+                label="Change e-mail"
+                label-for="email">
                 <!-- Todo verify e-mail and show e-mail as verified -->
-                <input 
+                <b-form-input 
                     type="text" 
                     id="email" 
                     name="email" 
                     placeholder="E-mail" 
                     v-model="emailSettings.email" />
-            </div>
+                <base-form-error name="email" /> 
+            </b-form-group>
             <div class="form-group">
                 <b-button type="submit" block>Update e-mail</b-button>
             </div>
@@ -105,7 +116,13 @@
 
 <script>
 import {mapGetters} from 'vuex';
+import BaseFormError from '../components/BaseFormError';
+import { REWARD_TYPES } from '../constants/rewardConstants';
+
 export default {
+    components: {
+        BaseFormError
+    },
     mounted() {
         this.setupSettings();
     },
@@ -116,6 +133,7 @@ export default {
             emailSettings: {},
             passwordSettings: {},
             loading: true,
+            rewardTypes: REWARD_TYPES,
         }
     },
     computed: {
