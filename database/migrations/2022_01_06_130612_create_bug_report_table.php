e69155fcf2fa6cb78ff5381b5e328241e0c2cc70
dmitritruf <gttim2@gmail.com>
2022-01-06 14:15:38 +0100

    created database table via migration

---
+++ database/migrations/2022_01_06_130612_create_bug_report_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBugReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bug_report', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('page');
+            $table->string('type');
+            $table->integer('severity');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('comment');
+            $table->string('admin_comment')->nullable();
+            $table->integer('status')->default(0); //0 = reported, 1 = in progress, 2 = done, 3 = resolved
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bug_report');
+    }
+}
