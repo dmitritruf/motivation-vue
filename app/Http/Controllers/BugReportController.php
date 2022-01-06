3115c236210339a302711bf01ef822fb69487840
dmitritruf <gttim2@gmail.com>
2022-01-06 14:54:59 +0100

    created store function in BugReportController

--- app/Http/Controllers/BugReportController.php
+++ app/Http/Controllers/BugReportController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BugReport;
+use App\Http\BugReportRessource;
+use App\Http\Requests\StoreBugReportRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
 
 class BugReportController extends Controller
 {
-    //
+    public function store(StoreBugReportRequest $request): JsonResponse{
+        $validated = $request->validated();
+        $validated['user_id'] = Auth::user()->id;
+
+        BugReport::create($validated);
+
+        return new JsonResponse(['message' => ['message' => ['Bug Report successfully created.']]], Response::HTTP_OK);
+    }
 }
