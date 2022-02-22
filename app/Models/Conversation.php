9e4998ff2d3afaa1ed5944454653ee9ae46eb7f0
dmitritruf <gttim2@gmail.com>
2022-02-22 12:15:56 +0100

    Removed error log

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -37,8 +37,6 @@ class Conversation extends Model
     }
 
     public function getLastMessage() {
-        //Message::where('conversation_id', $this->conversation_id)->order_by('created_at', 'desc')->first();
-        error_log($this->visibleMessages()->sortByDesc('created_at')->first());
         return $this->visibleMessages()->sortByDesc('created_at')->first();
     }
 
